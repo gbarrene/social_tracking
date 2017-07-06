ffd406be4996bedcdc99906efbe43ec416fc1910
sabrine <sfaxsabrine@hotmail.com>
2017-07-06 17:33:46 +0200

    modification of coordinate in array creation for stability - visialization of body centroid and face centroid- vizualisation of possible match between face and bodies

--- synchronization/detectionExtraction.py
+++ synchronization/detectionExtraction.py
@@ -20,13 +20,6 @@ def detection_extraction(bagPath):
     # Creation of empty list thta will be fill by extracted information and then use to create a dataframe of useful info
     detectTime = []
     detectSeq = []
-    box2dList = []
-    xList = []
-    yList = []
-    wList = []
-    hList = []
-    heightList = []
-    distanceList = []
     
     for topic, msg, t in bag.read_messages(topics=['/detector/detections']):
         #print (str(msg).split('\n'))
@@ -34,70 +27,21 @@ def detection_extraction(bagPath):
         #for e in str(msg).split('\n'):
         #if 'Laurene' in e:
 
-        # Extract the detection field that start and end with a regular pattern 
-        begin = str(msg).find('detections:')
-        end = str(msg).find('intrinsic_matrix:')
-        detections = str(msg)[begin:end]
-
-        # Extract the several detection for this msg
-        detection = detections.split("- ")
-    
-        #A non empty detection will be split in more than 1 cell
-        if len(detection) > 1:
-            for i in range(1, len(detection)):
                 
-                # Extract box2d coordinates
-                b = detection[i].find('box_2D:')
-                e = detection[i].find('centroid:')
-                box2D = detection[i][b:e]
-                box2dList.append(box2D)
-                match = re.findall('(\d+)\n', box2D)
-                x = match[0]
-                xList.append(x)
-                y = match[1]
-                yList.append(y)
-                w = match[2]
-                wList.append(w)
-                h = match[3]
-                hList.append(h)
+        # Extract timestamp
+        time = msg.header.stamp
+        detectTime.append(int(str(time)))
                 
-                # Reduce information to avoid ambiguious match
-                newdetection = detection[i][e:]
-                
-                # Extract height of the detection
-                b = newdetection.find('height:')
-                e = newdetection.find('confidence:')
-                h = re.search('\d+\.\d+',newdetection[b:e]).group(0)
-                heightList.append(float(h))
-                
-                # Reduce information to avoid ambiguious match
-                newdetectionre = newdetection[e:]
-                
-                # Extract distance of the detection
-                b = newdetectionre.find('distance:')
-                e = newdetectionre.find('occluded:')
-                d = re.search('\d+\.\d+',newdetectionre[b:e]).group(0)
-                distanceList.append(float(d))
-                
-                # Extract timestamp
-                time = msg.header.stamp
-                detectTime.append(int(str(time)))
-                
-                #Extract seq number
-                seq = msg.header.seq
-                detectSeq.append(seq)
-    
-    
-    
+        #Extract seq number
+        seq = msg.header.seq
+        detectSeq.append(int(seq))
+        
     #Creation of a dataframe of extracted information
-    data = np.array([np.asarray(detectTime), np.asarray(detectSeq), 
-            np.asarray(xList), np.asarray(yList), np.asarray(wList), 
-            np.asarray(hList), np.asarray(heightList), 
-            np.asarray(distanceList)]).T # The array need to be transposed
+    data = np.array([np.asarray(detectTime),
+     np.asarray(detectSeq)]).T # The array need to be transposed
+     
     detectionExtraction = pd.DataFrame(data=data)
-    detectionExtraction.columns = ['detectionTimestamp', 'seq', 
-            'detectionX', 'detectionY', 'detectionW', 'detectionH',
-             'height', 'distance']
+    detectionExtraction.columns = ['detectionTimestamp', 'detecSeq']
             
     return detectionExtraction
     
